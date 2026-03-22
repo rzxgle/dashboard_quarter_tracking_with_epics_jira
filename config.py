0bfc8a57c5313f68e0e0f3a1af988c2a2d9eae3d
glegle <39626338+rzxgle@users.noreply.github.com>
2026-03-22 14:50:49 -0300

    add fallback erro on envrionment variables

--- config.py
+++ config.py
@@ -11,5 +11,8 @@ JIRA_URL = os.getenv("JIRA_URL")
 JIRA_EMAIL = os.getenv("JIRA_EMAIL")
 JIRA_TOKEN = os.getenv("JIRA_TOKEN")
 
+if not all([JIRA_URL, JIRA_EMAIL, JIRA_TOKEN]):
+    raise ValueError("Missing environment variables for Jira connection")
+
 EPIC_LINK_FIELD = "customfield_10006"
 TEAM_FIELD = "customfield_10001"
